fff1c388ebce0a826ab2463ade15171612491f0b
bohdanmirchuk <bohdanmirchukslu@ukr.net>
2017-09-03 01:57:25 +0300

    checkers were added

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,2 +1,17 @@
 var name = "Yosyp", time = "tomorrow at 11:00", place = "Geneve", event = prompt("Enter the activity");
-console.log(name, "is", event, time, "in", place)
+if (event.match('ing')){
+	console.log(name, "is", event, time, "in", place)}
+	else{
+		alert("try to enter some activity in gerund form")
+	}
+
+
+function yosypEvent(){
+	var name = "Yosyp", time = "tomorrow at 11:00", place = "Geneve", event = prompt("Enter the activity");
+if (event.match('ing')){
+	console.log(name, "is", event, time, "in", place)}
+	else{
+		alert("Please try to enter some activity in gerund form")
+	}
+
+}
--- scripts/script2.js
+++ scripts/script2.js
@@ -1,12 +1,22 @@
 function currencyConventer(){
-	var eurQ = prompt("Please, the quantity of cass in euros", 100);
-	var dolQ = prompt("Please, the quantity of cass in dollars", 100);
-	var eurRate = 30.65;
-	var dolRate = 25.44;
-	var eur_grn = eurQ * eurRate;
-	var dol_grn = dolQ * dolRate;
-	var eur_dol = eurRate / dolRate;
-	alert(eurQ + ' euros are equal ' + eur_grn.toFixed(2) + ' gryvnias, ' + dolQ + ' dollars are equal ' + dol_grn.toFixed(2) + ' gryvnias, one euro is equal ' + eur_dol.toFixed(2) + ' dollars.');
+	var eurQ = prompt("Please, enter the amount of cass in euros", 100);
+	
+	var dolQ = prompt("Please, enter the amount of cash in dollars", 100);
+
+    if (isNaN(eurQ) || isNaN(dolQ)){
+    	alert("Hey, buddy, take another chance. But this time you need to enter numbers)")}
+    	else{
+    		theResult();
+    	}
+    	
+    function theResult(){
+    	var eurRate = 30.65;
+	    var dolRate = 25.44;
+	    var eur_grn = eurQ * eurRate;
+	    var dol_grn = dolQ * dolRate;
+	    var eur_dol = eurRate / dolRate;
+	    alert(eurQ + ' euros are equal ' + eur_grn.toFixed(2) + ' gryvnias, ' + dolQ + ' dollars are equal ' + dol_grn.toFixed(2) + ' gryvnias, one euro is equal ' + eur_dol.toFixed(2) + ' dollars.');
+    }
 }
 
 // 30.65
